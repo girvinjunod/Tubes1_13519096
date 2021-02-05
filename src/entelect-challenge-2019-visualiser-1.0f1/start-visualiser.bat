97562fefa822e78397bb9d9e10871691bf07be74
leo.matt.547 <leo.matt.547@gmail.com>
2021-02-05 13:36:50 +0700

    update game

--- src/entelect-challenge-2019-visualiser-1.0f1/start-visualiser.bat
+++ src/entelect-challenge-2019-visualiser-1.0f1/start-visualiser.bat
@@ -1,1 +1,1 @@
-entelect-visualiser.exe --round-step "1" --step-time "1" --replay-match "Matches/2019.07.30.10.00.20" -screen-width 1920 -screen-height 1080 -screen-quality Ultra
+entelect-visualiser.exe --round-step "1" --step-time "1" --replay-match "2021.02.05.11.53.57" -screen-width 1920 -screen-height 1080 -screen-quality Ultra
